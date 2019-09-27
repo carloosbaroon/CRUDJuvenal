b810b34fdea9aa9be6e283fe4b95e2fb3d09ea53
Carlos Baron <carlostectol@gmail.com>
2019-09-27 07:49:56 -0500

    Duenos Bean

---
+++ VeterinariaWebApp/src/main/java/itesm/business/DuenoBean.java
@@ -0,0 +1,35 @@
+package itesm.business;
+
+public class DuenoBean {
+	public String nombre;
+	public String direccion;
+	public int telefono;
+	public String rfc;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public String getRfc() {
+		return rfc;
+	}
+	public void setRfc(String rfc) {
+		this.rfc = rfc;
+	}
+
+	
+}
