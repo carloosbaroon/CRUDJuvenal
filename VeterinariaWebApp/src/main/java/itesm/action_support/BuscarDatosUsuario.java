3b89743f1551e5ff529dc8bd48b7f4b1e73aaa28
Riper32 <nagato32@outlook.es>
2019-09-28 11:00:17 -0500

    some changes baja de usuario

--- VeterinariaWebApp/src/main/java/itesm/action_support/BuscarDatosUsuario.java
+++ VeterinariaWebApp/src/main/java/itesm/action_support/BuscarDatosUsuario.java
@@ -15,44 +15,30 @@ public class BuscarDatosUsuario extends ActionSupport implements SessionAware{
 	private EmpleadoBean empleado;
 	private UsuarioBean usuario;
 	private Map<String, Object> session_map;
-	private ArrayList<EmpleadoBean> buffer_empleados;
-	private ArrayList<UsuarioBean> buffer_usuarios;
-	private String message;
+	private ArrayList<EmpleadoBean> buffer_empleados, buffer_empleados_aux;
+	private ArrayList<UsuarioBean> buffer_usuarios, buffer_usuarios_aux;
+	
+	public UsuarioBean getUser() {
+		return usuario;
+	}
+	//private String message;
 	
 	public String execute() {
-		//1. Obtener la tabla usuario con todos sus registros
-		this.buffer_usuarios = (ArrayList<UsuarioBean>)this.session_map.get(Tabla.TABLA_USUARIO);
-		//1. Obtener la tabla usuario con todos sus registros
-		this.buffer_empleados = (ArrayList<EmpleadoBean>)this.session_map.get(Tabla.TABLA_EMPLEADO);
 		
-		System.out.println(buffer_usuarios);
+		//1. Recuperamos el ArrayList de Usuarios y el de Empleados accediendo a Map
+		this.buffer_usuarios = (ArrayList<UsuarioBean>)this.session_map.get(Tabla.TABLA_USUARIO);
+		this.buffer_empleados = (ArrayList<EmpleadoBean>)this.session_map.get(Tabla.TABLA_EMPLEADO);
+		//Recorremos el buffer de empleados y filtramos(Nos quedamos con todos los empleados que no han sido elegidos)
+		buffer_usuarios_aux = new ArrayList<UsuarioBean>();
+		for(EmpleadoBean item : this.buffer_empleados) {
+			if(item.getNo_empleado().equals(usuario.getUsuarioID()))//Si el usuario tiene asociada la clave del empleado
+			{
+				
+			}
+		}
 		
 		return SUCCESS;
 	}
-	/*public String execute() {
-		usuario = (UsuarioBean)session_map.get(usuario.getUsuarioID());
-		if(usuario == null)
-		{
-			message = "No existe el usuario requerido";
-			return ERROR;
-		}
-			return SUCCESS;
-	
-	}*/
-	
-	/*public String show() {
-		if(!pacienteSession.containsKey("userCount")){
-			pacienteSession.put("userCount", pacienteCount);
-		}
-		pacienteCount = (Integer) pacienteSession.get("userCount");
-		
-		pacienteCount ++;
-		pacienteSession.put("userCount", pacienteCount);
-		
-		paciente.setId((String.valueOf(pacienteCount)));
-		pacienteSession.put(paciente.getId(), paciente);	
-		return SUCCESS;
-	}*/
 	
 	public ArrayList<EmpleadoBean> getBuffer_empleados() {
 		return buffer_empleados;
@@ -68,47 +54,3 @@ public class BuscarDatosUsuario extends ActionSupport implements SessionAware{
 	}
 
 }
-
-/*
-	private static final long serialVersionUID = 1L;
-	private PacienteBean paciente;
-	private int pacienteCount;
-	private Map<String, Object> pacienteSession;
-	private String message;
-	
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public PacienteBean getPaciente() {
-		return paciente;
-	}
-
-	public void setPaciente(PacienteBean paciente) {
-		this.paciente = paciente;
-	}
-
-	public int getPacienteCount() {
-		return pacienteCount;
-	}
-
-	public void setPacienteCount(int pacienteCount) {
-		this.pacienteCount = pacienteCount;
-	}
-
-	public void setSession(Map<String, Object> session) {
-		pacienteSession = session;
-		
-	}
-	
-	public String init() {
-		return SUCCESS;
-	}
-	
-	
-
-}*/
