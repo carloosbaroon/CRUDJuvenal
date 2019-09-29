97dbb9068e11d741273c3eb8865c375c8516057d
Sergio <a01365942@itesm.mx>
2019-09-28 23:08:16 -0500

    La modificación de estados de usuario funciona

--- VeterinariaWebApp/src/main/java/itesm/action_support/editarStatusUsuario.java
+++ VeterinariaWebApp/src/main/java/itesm/action_support/editarStatusUsuario.java
@@ -14,7 +14,15 @@ public class editarStatusUsuario extends ActionSupport implements SessionAware {
 	private UsuarioBean usuario;
 	private Map<String, Object> usuarioSession;
 	private ArrayList<UsuarioBean> buffer_usuarios;
-	private String message;
+	private ArrayList<String> list_estado;
+	public ArrayList<String> getList_estado() {
+		return list_estado;
+	}
+	public void setList_estado(ArrayList<String> list_estado) {
+		this.list_estado = list_estado;
+	}
+
+	private String message = "No existe el usuario requerido";
 	
 	public UsuarioBean getUsuario() {
 		return usuario;
@@ -39,20 +47,41 @@ public class editarStatusUsuario extends ActionSupport implements SessionAware {
 	
 	public String execute() {
 		this.buffer_usuarios = (ArrayList<UsuarioBean>)this.usuarioSession.get(Tabla.TABLA_USUARIO);
-		
-		for(UsuarioBean item: buffer_usuarios) {
-			if(item.getUsuarioID().equals(usuario.getUsuarioID())){
-				return SUCCESS;
+		if(this.buffer_usuarios == null) 
+			return ERROR;
+		else {
+			for(UsuarioBean item: buffer_usuarios) {
+				if(item.getUsuarioID().equals(usuario.getUsuarioID())){
+					this.list_estado = new ArrayList<String>();
+					this.list_estado.add("Activo");
+					this.list_estado.add("Bloqueado");
+					this.list_estado.add("Inactivo");
+					//Guardamos en la variable de sesión el ID de Usuario para que más adelante lo ocupemos
+					this.usuarioSession.put("user_id", usuario.getUsuarioID());//Se guarda de forma ineficiente
+					return SUCCESS;
+				}
 			}
-		}
-			message = "No existe el usuario requerido";
+			
+			
+		}			
 			return ERROR;
 		
 		
 	}
 	
 	public String show() {
-		usuarioSession.put(usuario.getUsuarioID(),usuario.getEstado());
+		String num = (String)this.usuarioSession.get("user_id");
+		System.out.println("Numero de Sesion: " + num);//Se obtiene de forma ineficiente
+		this.buffer_usuarios = (ArrayList<UsuarioBean>)this.usuarioSession.get(Tabla.TABLA_USUARIO);
+		for(UsuarioBean item : this.buffer_usuarios) {
+			if(item.getUsuarioID().equals(this.usuarioSession.get("user_id"))) {
+				//Obtenemos el usuario que queremos actulizarle el estado y lo actualizamos
+				item.setEstado(usuario.getEstado());
+			}
+		}
+		
+		this.usuarioSession.put(Tabla.TABLA_USUARIO, this.buffer_usuarios);
+		
 		return SUCCESS;
 	}
 	
--- VeterinariaWebApp/src/main/webapp/WEB-INF/admin/crud_usuarios/validarIDStatus.jsp
+++ VeterinariaWebApp/src/main/webapp/WEB-INF/admin/crud_usuarios/validarIDStatus.jsp
@@ -7,7 +7,7 @@
 
 <body>
 <s:form action="modificarStatus">
-    <s:textfield name="usuario.usuarioID" label="Id Usuario" />
+    <s:textfield name="usuario.usuarioID" label="Id Usuario"><s:param name="id_usuario" value="usuario.usuarioID"/></s:textfield>
     <s:submit value="Enviar" />
 </s:form>
 </body>
