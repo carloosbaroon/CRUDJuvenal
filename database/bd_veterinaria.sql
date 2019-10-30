505032b8f59afd43e3f982910cbe9fd98a0c8410
Sergio <a01365942@itesm.mx>
2019-10-29 21:30:00 -0600

    Se agrego la carpeta 'database', con scripts y diagrama E-R
    El diagrama se puede visualizar con Toad Data Modeler

---
+++ database/bd_veterinaria.sql
@@ -0,0 +1,86 @@
+/*
+Created		26/10/2019
+Modified		26/10/2019
+Project		
+Model		
+Company		
+Author		
+Version		
+Database		mySQL 5 
+*/
+
+create database veterinaria;
+use veterinaria;
+
+drop table IF EXISTS Consultas;
+drop table IF EXISTS Sala;
+drop table IF EXISTS Paciente;
+drop table IF EXISTS Propietario;
+drop table IF EXISTS Empleado;
+drop table IF EXISTS Usuario;
+
+
+Create table Usuario (
+	id_usuario Int NOT NULL AUTO_INCREMENT,
+	id_empleado Int NOT NULL,
+	password_user Varchar(30) NOT NULL,
+	confirmar_password Varchar(30) NOT NULL,
+	grupo Varchar(20) NOT NULL,
+	estado Varchar(20) NOT NULL,
+	no_intentos Int,
+	rol Int NOT NULL,
+ Primary Key (id_usuario)) ENGINE = MyISAM;
+
+Create table Empleado (
+	id_empleado Int NOT NULL AUTO_INCREMENT,
+	nombre_completo Varchar(30),
+	direccion Varchar(30) NOT NULL,
+	telefono Varchar(20) NOT NULL,
+	puesto Varchar(30) NOT NULL,
+	especialidad Varchar(20) NOT NULL,
+	turno Varchar(20) NOT NULL,
+	estado Varchar(20) NOT NULL,
+ Primary Key (id_empleado)) ENGINE = MyISAM;
+
+Create table Propietario (
+	id_propietario Int NOT NULL AUTO_INCREMENT,
+	nombre Varchar(30) NOT NULL,
+	direccion Varchar(30) NOT NULL,
+	telefono Varchar(20) NOT NULL,
+	rfc Varchar(20) NOT NULL,
+ Primary Key (id_propietario)) ENGINE = MyISAM;
+
+Create table Paciente (
+	id_paciente Int NOT NULL,
+	id_propietario Int NOT NULL,
+	nombre Varchar(30) NOT NULL,
+	categoria Varchar(20) NOT NULL,
+	raza Varchar(20) NOT NULL,
+	edad Int NOT NULL,
+ Primary Key (id_paciente)) ENGINE = MyISAM;
+
+Create table Sala (
+	id_sala Int NOT NULL AUTO_INCREMENT,
+	nombre Varchar(20) NOT NULL,
+	disponibilidad Int NOT NULL,
+ Primary Key (id_sala)) ENGINE = MyISAM;
+
+Create table Consultas (
+	fecha Date NOT NULL,
+	id_empleado Int NOT NULL,
+	id_sala Int NOT NULL,
+	id_paciente Int NOT NULL,
+	hora_inicial Time NOT NULL,
+	hora_final Time NOT NULL) ENGINE = MyISAM;
+
+
+Alter table Usuario add Foreign Key (id_empleado) references Empleado (id_empleado) on delete  restrict on update  restrict;
+Alter table Consultas add Foreign Key (id_empleado) references Empleado (id_empleado) on delete  restrict on update  restrict;
+Alter table Paciente add Foreign Key (id_propietario) references Propietario (id_propietario) on delete  restrict on update  restrict;
+Alter table Consultas add Foreign Key (id_paciente) references Paciente (id_paciente) on delete  restrict on update  restrict;
+Alter table Consultas add Foreign Key (id_sala) references Sala (id_sala) on delete  restrict on update  restrict;
+
+
+/* Users permissions */
+
+
---
+++ database/insertions_usuarios.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuario (nombre_completo, direccion, telefono, puesto, especialidad, turno, estado) 
+VALUES ("Paola", "CU", "987654321", "puesto de prueba 3", "no se 2", "3", "Activo");
