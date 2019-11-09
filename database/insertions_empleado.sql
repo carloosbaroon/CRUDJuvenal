6e9bca62b04f9a392133754bc63c6acdb4572616
Sergio <a01365942@itesm.mx>
2019-11-09 10:57:07 -0600

    Se eliminaron archivos inutiles SQL

--- database/insertions_empleado.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO empleado (nombre_completo, direccion, telefono, puesto, especialidad, turno, estado) 
-VALUES ("David", "Av de la vida", "7226483059", "puesto de prueba 1", "no se", "1", "Activo");
-
-INSERT INTO empleado (nombre_completo, direccion, telefono, puesto, especialidad, turno, estado) 
-VALUES ("Mariana", "Av las torres", "123456789", "puesto de prueba 2", "no se 2", "2", "Baja");
-
-INSERT INTO empleado (nombre_completo, direccion, telefono, puesto, especialidad, turno, estado) 
-VALUES ("Paola", "CU", "987654321", "puesto de prueba 3", "no se 2", "3", "Activo");
