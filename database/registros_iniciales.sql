d1313372761da4c4890621660d1e9c63c6d60ded
Sergio <a01365942@itesm.mx>
2019-11-09 11:57:46 -0600

    Modificaciones a un script sql y correcion de errores ->
    - En el formulario 'Alta dueño' se reedirige a un jsp de error

--- database/registros_iniciales.sql
+++ database/registros_iniciales.sql
@@ -1,13 +1,13 @@
 use veterinaria;
 
-INSERT INTO empleado (nombre_completo, direccion, telefono, puesto, especialidad, turno, estado) 
-VALUES ("David", "Av de la vida", "7226483059", "puesto de prueba 1", "no se", "1", "Activo");
+INSERT INTO empleado (nombre_completo, direccion, telefono, puesto, especialidad, turno, estado, elegido) 
+VALUES ("David", "Av de la vida", "7226483059", "puesto de prueba 1", "no se", "1", "Activo", 1);
 
-INSERT INTO empleado (nombre_completo, direccion, telefono, puesto, especialidad, turno, estado) 
-VALUES ("Mariana", "Av las torres", "123456789", "puesto de prueba 2", "no se 2", "2", "Baja");
+INSERT INTO empleado (nombre_completo, direccion, telefono, puesto, especialidad, turno, estado, elegido) 
+VALUES ("Mariana", "Av las torres", "123456789", "puesto de prueba 2", "no se 2", "2", "Baja", elegido, 1);
 
-INSERT INTO empleado (nombre_completo, direccion, telefono, puesto, especialidad, turno, estado) 
-VALUES ("Paola", "CU", "987654321", "puesto de prueba 3", "no se 2", "3", "Activo");
+INSERT INTO empleado (nombre_completo, direccion, telefono, puesto, especialidad, turno, estado, elegido) 
+VALUES ("Paola", "CU", "987654321", "puesto de prueba 3", "no se 2", "3", "Activo", 1);
 
 INSERT INTO usuario (id_empleado, password_user, confirmar_password, privilegios, estado, no_intentos) 
 VALUES (1, "hola", "hola", "Administrador", "activo", 5);
