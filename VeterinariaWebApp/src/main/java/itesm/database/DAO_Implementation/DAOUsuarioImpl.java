15e133f70fec5e0a87bf34b6d72b3aa6b2c207ee
Sergio <a01365942@itesm.mx>
2019-11-09 09:24:11 -0600

    El sistema de Bloqueo/Desbloqueo nivel de usuario y admin funciona
    Caracteristicas:
    - Establecer un valor por defecto al campo 'no_intentos' cuando se crea un usuario
    - Actualizar campo 'no_intentos' cuando falle en ingresar el usuario
    - En caso de que el usuario haya ingresado correctamente su password tras varios intentos fallidos(Máximo 4),
    el contador de número de intentos se establece a 4.
    - Cuando el número de intentos del usuario llegue a 0 se modifica el estado del usuario a 'bloqueado'
    - Cuando el usuario bloqueado trate de ingresar a su cuenta a pesar de que esta bloqueada se le debera mostrar un mensaje
    - Cuando el administrador quiera desbloquear a un Usuario, el usuario vuelve a tener 4 intentos

--- VeterinariaWebApp/src/main/java/itesm/database/DAO_Implementation/DAOUsuarioImpl.java
+++ VeterinariaWebApp/src/main/java/itesm/database/DAO_Implementation/DAOUsuarioImpl.java
@@ -29,7 +29,6 @@ public class DAOUsuarioImpl extends Conexion implements DAOUsuario{
         
         //If column 'empleado_id_fk' is not null, set the value 1 to the col 'elegido'
         if(usuario.getId_empleado_FK() != null) {
-        	System.out.println("No esta vacio el campo 'empleado_id_fk'");
         	String sqlUpdate = "UPDATE empleado SET elegido = 1 WHERE id_empleado = ?";
         	PreparedStatement ps = conn.prepareStatement(sqlUpdate);
         	ps.setString(1, usuario.getId_empleado_FK());
@@ -43,7 +42,7 @@ public class DAOUsuarioImpl extends Conexion implements DAOUsuario{
         ps.setString(4, usuario.getConfirmar_password());
         ps.setString(5, usuario.getPrivilegios());
         ps.setString(6, usuario.getEstado());
-        ps.setString(7, usuario.getIntentos());
+        ps.setString(7, Integer.toString(4));
         
         ps.execute();
 
@@ -57,26 +56,25 @@ public class DAOUsuarioImpl extends Conexion implements DAOUsuario{
 	}
 
 	@Override
-	public String verificarUsuario(UsuarioBean usuario) throws Exception {	
-		String ret = "error";
+	public UsuarioBean verificarUsuario(UsuarioBean usuario) throws Exception {	
 	      Connection conn = null;
 	      
 	    	 establishConnection();
 	         conn = getCon();
-	         String sql = "SELECT id_usuario, privilegios FROM usuario WHERE";
+	         String sql = "SELECT id_usuario, privilegios, no_intentos, estado FROM usuario WHERE";
 	         sql+=" id_usuario = ? AND password_user = ?";
 	         PreparedStatement ps = conn.prepareStatement(sql);
 	         ps.setString(1, usuario.getUsuarioID());
 	         ps.setString(2, usuario.getPassword());
 	         ResultSet rs = ps.executeQuery();
-
+         	
+	         UsuarioBean usuarioAux = null;
 	         while (rs.next()) {
-	            usuario.setUsuarioID((rs.getString(1)));
-	            usuario.setPrivilegios(rs.getString(2));
-	            if(usuario.getPrivilegios().equals("Administrador"))
-	            	return "admin";
-            	else
-            		return "user";
+	        	 usuarioAux = new UsuarioBean();
+	            usuarioAux.setUsuarioID(rs.getString(1));
+	            usuarioAux.setPrivilegios(rs.getString(2));
+	            usuarioAux.setIntentos(rs.getString(3));
+	            usuarioAux.setEstado(rs.getString(4));
 	         }
 
 	         if (conn != null) {
@@ -87,7 +85,7 @@ public class DAOUsuarioImpl extends Conexion implements DAOUsuario{
 	            }
 	         }
 	      
-	      return ret;
+	      return usuarioAux;
 	}
 
 	@Override
@@ -213,8 +211,6 @@ public class DAOUsuarioImpl extends Conexion implements DAOUsuario{
 	      
    	 	establishConnection();
         conn = getCon();
-        System.out.println("Usuario: " + usuario.getUsuarioID());
-        System.out.println("Estado: " + usuario.getEstado());
         
         String sql = "UPDATE usuario SET estado = ? WHERE id_usuario = ?";
                 
@@ -232,5 +228,59 @@ public class DAOUsuarioImpl extends Conexion implements DAOUsuario{
            }
         }		
 		
+	}
+
+	@Override
+	public void actualizarIntentos(UsuarioBean usuario) throws Exception {
+		Connection conn = null;
+	      
+   	 	establishConnection();
+        conn = getCon();
+        
+        String sql = "UPDATE usuario SET no_intentos = ? WHERE id_usuario = ?";
+                
+        PreparedStatement ps = conn.prepareStatement(sql);
+        
+        int intentosRestantes = Integer.parseInt(usuario.getIntentos()) - 1;
+        ps.setString(1, Integer.toString(intentosRestantes));
+        ps.setString(2, usuario.getUsuarioID());
+        
+        ps.execute();
+
+        if (conn != null) {
+           try {
+              closeConnection();
+           } catch (Exception e) {
+				e.printStackTrace();
+           }
+        }		
+	}
+
+	@Override
+	public boolean confirmarExistenciaID(String id) throws Exception {
+		Connection conn = null;
+	      
+   	 	establishConnection();
+        conn = getCon();
+        String sql = "SELECT * FROM usuario WHERE";
+        sql+=" id_usuario = ?";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, id);
+        ResultSet rs = ps.executeQuery();
+        
+        UsuarioBean usuario = null;
+        
+        while (rs.next()) {
+        	return true;
+        }
+
+        if (conn != null) {
+           try {
+              closeConnection();
+           } catch (Exception e) {
+				e.printStackTrace();
+           }
+        }
+		return false;
 	}
 }
--- VeterinariaWebApp/src/main/java/itesm/database/DAO_Interfaces/DAOUsuario.java
+++ VeterinariaWebApp/src/main/java/itesm/database/DAO_Interfaces/DAOUsuario.java
@@ -7,7 +7,9 @@ import itesm.business.UsuarioBean;
 public interface DAOUsuario extends CRUD<UsuarioBean>{
 	//Este método ayuda a verificar que el usuario que quiera autenticarse exista en la BD y en caso de existir
 	//indica a que menu principal se le tiene que reedirigr
-	public String verificarUsuario(UsuarioBean usuario) throws Exception;
+	public UsuarioBean verificarUsuario(UsuarioBean usuario) throws Exception;
+	public boolean confirmarExistenciaID(String id) throws Exception;
 	public void eliminarUsuario(UsuarioBean usuario) throws Exception;
 	public void actualizarEstado(UsuarioBean usuario) throws Exception;
+	public void actualizarIntentos(UsuarioBean usuario) throws Exception;
 }
