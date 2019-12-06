7ec943a36d5dd76e2d99b1a06c203cf2183988d0
Riper32 <nagato32@outlook.es>
2019-12-06 14:27:28 -0600

    Lo basico de atenciones

--- VeterinariaWebApp/src/main/java/itesm/business/AtencionBean.java
+++ VeterinariaWebApp/src/main/java/itesm/business/AtencionBean.java
@@ -1,5 +1,56 @@
 package itesm.business;
 
 public class AtencionBean {
-
+	private String id_atencion;
+	private String id_consulta;
+	private String id_paciente;
+	private String hora_entrada;
+	private String hora_salida;
+	private String detalle;
+	private String monto;
+	
+	
+	public String getId_atencion() {
+		return id_atencion;
+	}
+	public void setId_atencion(String id_atencion) {
+		this.id_atencion = id_atencion;
+	}
+	public String getId_consulta() {
+		return id_consulta;
+	}
+	public void setId_consulta(String id_consulta) {
+		this.id_consulta = id_consulta;
+	}
+	public String getId_paciente() {
+		return id_paciente;
+	}
+	public void setId_paciente(String id_paciente) {
+		this.id_paciente = id_paciente;
+	}
+	public String getHora_entrada() {
+		return hora_entrada;
+	}
+	public void setHora_entrada(String hora_entrada) {
+		this.hora_entrada = hora_entrada;
+	}
+	public String getHora_salida() {
+		return hora_salida;
+	}
+	public void setHora_salida(String hora_salida) {
+		this.hora_salida = hora_salida;
+	}
+	public String getDetalle() {
+		return detalle;
+	}
+	public void setDetalle(String detalle) {
+		this.detalle = detalle;
+	}
+	public String getMonto() {
+		return monto;
+	}
+	public void setMonto(String monto) {
+		this.monto = monto;
+	}
+	
 }
---
+++ VeterinariaWebApp/src/main/java/itesm/database/DAO_Implementation/DAOAtencionImpl.java
@@ -0,0 +1,35 @@
+package itesm.database.DAO_Implementation;
+
+import java.util.ArrayList;
+
+import itesm.business.AtencionBean;
+import itesm.database.Conexion;
+import itesm.database.DAO_Interfaces.DAOAtencion;
+
+public class DAOAtencionImpl extends Conexion implements DAOAtencion {
+
+	@Override
+	public void insertar(AtencionBean entidad) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public AtencionBean buscar(String id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void editar(AtencionBean entidad) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<AtencionBean> consultar() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
